f616f9fb79b490330c2e9cc672f0aa4bcbda18a3
Dmitri Smith <dmitrismith5@gmail.com>
2020-07-02 04:56:57 -0500

    Created a basic terminal gui. Algorithm is horribly inefficient, will be revisted soon.

--- src/board.rs
+++ src/board.rs
@@ -5,18 +5,83 @@ pub struct Board
 {
     width: u8,
     height: u8,
-    bots: Vec<kilobot::Kilobot>,
+    bots: Vec<BotLocation>,
 }
 
+//TODO: REWORK print_board ASAP!
 impl Board
 {
-    pub fn init(&mut self, width: u8, height: u8) -> &mut Board
+    //Add new bot to the board
+    //TODO: Sort bots
+    pub fn add_bot(&mut self, bot: kilobot::Kilobot, x: u8, y: u8)
     {
-        self.width = width;
-        self.height = heigth;
-        bots.clear;
-        self
+        let mut space_available: bool = true;
+        if x >= self.width || y >= self.height
+        {
+            space_available = false;
+        }
+        for i in 0..self.bots.len()
+        {
+            if self.bots.get(i).unwrap().x == x && self.bots.get(i).unwrap().y == y
+            {
+                space_available = false;
+                break;
+            }
+        }
+        if space_available
+        {
+            self.bots.push(BotLocation {x,y,bot})
+        }
+
     }
+
+    //Print left to right, top to bottom
+    pub fn print_board(&self)
+    {
+        for i in 0..self.height
+        {
+            for j in 0..self.width
+            {
+                let mut bot_here = false;
+                for k in 0..self.bots.len()
+                {
+
+                    if self.bots.get(k).unwrap().x == j && self.bots.get(k).unwrap().y == i
+                    {
+                        bot_here = true;
+                        break;
+                    }
+                }
+
+                if bot_here
+                {
+                    print!("O ");
+                }
+                else
+                {
+                    print!("* ");
+                }
+            }
+            print!("\n");
+        }
+    }
+}
+
+struct BotLocation
+{
+    pub x: u8,
+    pub y: u8,
+    pub bot: kilobot::Kilobot,
+}
+
+impl BotLocation
+{
+
+}
+
+pub fn new_board(width: u8, height: u8) -> Board
+{
+    Board {width, height, bots: vec![] }
 }
 
 impl fmt::Display for Board
@@ -27,9 +92,4 @@ impl fmt::Display for Board
                , self.height
                , self.bots.len())
     }
-}
-
-pub fn new_board(width: u8, height: u8) -> Board
-{
-    init(width, height)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let bot_radius = 5;
     let mut new_bot = kilobot::new_kilobot(0,bot_radius);
     test_bot(&mut new_bot);
-
+    let mut new_board = board::new_board(5, 5);
+    test_board(&mut new_board);
 }
 
 fn test_bot(bot: &mut kilobot::Kilobot)
@@ -17,7 +18,16 @@ fn test_bot(bot: &mut kilobot::Kilobot)
     assert_eq!(bot.get_motor_values(), (0,0));
     bot.move_forward();
     assert_eq!(bot.get_motor_values(), (kilobot::MOTOR_MAX_VAL, kilobot::MOTOR_MAX_VAL));
-
 }
+
+fn test_board(board: &mut board::Board)
+{
+    board.add_bot((kilobot::new_kilobot(1,1)),0,0);
+    board.add_bot((kilobot::new_kilobot(1,1)),0,0);
+    board.add_bot((kilobot::new_kilobot(1,1)),2,2);
+    println!("Board: {}", board);
+    board.print_board();
+}
+
 
 
